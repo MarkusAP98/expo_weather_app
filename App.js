999559718a3243b166901cc5e4a81ddc5a4881bc
Markus <markus.a.phantha@gmail.com>
2024-05-15 15:27:12 +0300

    chore: Update App.js to include lazy loading for images and improve navigation styling

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Animated, StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+} from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { Winter, Spring, Summer, Fall } from "./components";
@@ -19,27 +27,23 @@ export default function App() {
 
   useEffect(() => {
     Animated.loop(
-      Animated.timing(
-        spinValue,
-        {
-          toValue: 1,
-          duration: 8000,
-          useNativeDriver: true
-        }
-      )
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 10000,
+        useNativeDriver: true,
+      })
     ).start();
   }, []);
 
   const spin = spinValue.interpolate({
     inputRange: [0, 1, 2],
-    outputRange: ['0deg', '360deg', "0deg"]
+    outputRange: ["0deg", "360deg", "0deg"],
   });
-
   return (
     <NavigationContainer styles={styles.navigation}>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home">
-          {props => <HomeScreen {...props} spin={spin} />}
+          {(props) => <HomeScreen {...props} spin={spin} />}
         </Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
