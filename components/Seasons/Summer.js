86bf2ea3f928f2bcbaf16ec4a6f84004dc6e2582
Markus <markus.a.phantha@gmail.com>
2024-05-15 13:48:05 +0300

    chore: Add cloud animation to Summer component

--- components/Seasons/Summer.js
+++ components/Seasons/Summer.js
@@ -1,12 +1,40 @@
 // components/Seasons/Summer.js
+import React, { useEffect, useRef } from "react";
 import { Animated, StyleSheet, Text, View } from "react-native";
 
 export default function Summer({ children, spin }) {
+  const cloudAnim = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(cloudAnim, {
+          toValue: 1,
+          duration: 5000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(cloudAnim, {
+          toValue: 0,
+          duration: 10000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
+
+  const cloudTranslate = cloudAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-20, 100], // adjust these values as needed
+  });
   return (
     <View style={styles.background}>
       {children}
       <Animated.Text style={{ ...styles.sun, transform: [{ rotate: spin }] }}>
         ☀️
+      </Animated.Text>
+      <Animated.Text
+        style={{ ...styles.cloud, transform: [{ translateX: cloudTranslate }] }}
+      >
+        ☁️
       </Animated.Text>
     </View>
   );
@@ -28,6 +56,12 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 10,
     right: 10,
+    fontSize: 60,
+  },
+  cloud: {
+    position: "absolute",
+    top: 60,
+    right: 50,
     fontSize: 60,
   },
 });
